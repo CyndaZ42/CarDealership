c8119c23161b575befe3a2aba3c2399de62e4880
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-07-20 10:12:22 -0700

    updoot

--- CarDealership/Controllers/HomeController.cs
+++ CarDealership/Controllers/HomeController.cs
@@ -1,20 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using CarDealership.Models;
+using System.Collections.Generic;
 
 namespace CarDealership.Controllers
 {
     public class HomeController : Controller
     {
-
       [HttpGet("/")]
       public ActionResult Index()
+      {
+        List<Car> allCars = Car.GetAllCars();
+        return View(allCars);
+      }
+
+      [HttpGet("/new")]
+      public ActionResult MakeCar()
       {
         return View();
       }
 
-      [HttpGet("/new")]
-      public ActionResult CreateForm()
+      [HttpPost("/showCars")]
+      public ActionResult DisplayCars(string makeModel, int price, int miles)
       {
-        return View();
+        Car myCar = new Car(makeModel, price, miles);
+        return RedirectToAction("Index");
       }
     }
 }
--- CarDealership/Models/Car.cs
+++ CarDealership/Models/Car.cs
@@ -1,19 +1,29 @@
 using System;
+using System.Collections.Generic;
 
-namespace Dealership.Models {
-
+namespace CarDealership.Models 
+{
   public class Car
   {
     public string MakeModel { get; set; }
     public int Price { get; set; }
     public int Miles { get; set; }
+    private static List<Car> _instances = new List<Car> { }; 
 
-    public Car(string makeModel, int price, int miles)
+    public static Car(string makeModel, int price, int miles)
     {
       MakeModel = makeModel;
       Price = price;
       Miles = miles;
+      _instances.Add(this);
     }
+
+    public static List<Car> GetAllCars()
+    {
+      return _instances;
+    }
+  }
+}
 /* 
     public bool WorthBuying(int maxPrice)
     {
@@ -29,5 +39,3 @@ namespace Dealership.Models {
     {
      return "Our cars sound like " + sound;
     } */
-  }
-}
